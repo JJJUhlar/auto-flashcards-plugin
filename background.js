fa4ce32e5d9a13f685d3ac61f956dd9b9e966fb6
JJJUhlar <jjuhlar@gmail.com>
2023-03-11 21:24:26 +0000

    added shortcut command and logic for badge text change

--- background.js
+++ background.js
@@ -3,3 +3,19 @@ chrome.runtime.onInstalled.addListener(() => {
       text: "OFF",
     });
   });
+
+if (nextState === "ON") {
+    // Insert the CSS file when the user turns the extension on
+    await chrome.scripting.insertCSS({
+    files: ["focus-mode.css"],
+    target: { tabId: tab.id },
+    });
+} else if (nextState === "OFF") {
+    // Remove the CSS file when the user turns the extension off
+    await chrome.scripting.removeCSS({
+    files: ["focus-mode.css"],
+    target: { tabId: tab.id },
+    });
+}
+  
+
