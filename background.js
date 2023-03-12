fe0efa408671c8ba4c9be946e086b73e0b956f40
JJJUhlar <jjuhlar@gmail.com>
2023-03-12 11:56:28 +0000

    added get selection text input from text highlight

--- background.js
+++ background.js
@@ -1,8 +1,31 @@
 chrome.runtime.onInstalled.addListener(() => {
     chrome.action.setBadgeText({
-      text: "OFF",
+      text: "ON",
     });
+   
+    
 });
+
+function selectGetter(info,tab) {
+  console.log(info.selectionText)
+  console.log(tab.url)
+  chrome.tabs.create({
+    url: `http://www.google.com/search?="${info.selectionText}"`
+  })
+}
+
+chrome.contextMenus.create({
+  title: "make flashcard for '%s'",
+  contexts: ['page','selection'],
+  type: "normal",
+  visible: true,
+  enabled: true,
+  id: "flashcardContextMenu"
+})
+
+chrome.contextMenus.onClicked.addListener(
+  selectGetter
+)
 
 // I will use a background service worker here to 
 // open the flashcard queue while navigating to a new tab and to throttle loading
--- scripts/content.js
+++ scripts/content.js
@@ -1,7 +1,2 @@
 // Content scripts will get the text payload content from a webpage by reading the DOM and send it to flashcard api
-
-document.oncontextmenu = () => {
-    let selection = document.getSelection();
-    let text = selection.toString();
-    console.log(text)
-}
+// maybe on every page insert 
