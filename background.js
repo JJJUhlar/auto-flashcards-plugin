9aaf48c508153632dc67ceb8ebf549ea26d2f805
JJJUhlar <jjuhlar@gmail.com>
2023-05-11 11:21:24 +0100

    fetch flashcards in popup

--- background.js
+++ background.js
@@ -20,15 +20,6 @@ chrome.runtime.onInstalled.addListener(() => {
   })
 });
 
-
-
-
-
-
-const input = {
-
-}
-
 chrome.contextMenus.onClicked.addListener(
   selectGetter
 )
@@ -36,40 +27,20 @@ chrome.contextMenus.onClicked.addListener(
 function selectGetter(info,tab) {
   console.log(info.selectionText)
   console.log(tab.url)
-  console.log(typeof info.selectionText)
-  console.log(String(info.selectionText).length)
-
   
-  if (String(info.selectionText).length > 0 && String(info.selectionText).length < 1000) {
-    fetch(`http://127.0.0.1:5000/flashcards`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        "url": tab.url,
-        "text": info.selectionText})
-      })
-      .then((res)=>{
-        console.log(res.json())
-      })
-      .catch((err)=>{
-        console.log(err)
-        console.log('error')
-      })
-    }
+  const input = {
+    "text": info.selectionText,
+    "origin": tab.url
+  }
 
-  input["textInput"] = info.selectionText;
-  input["textOrigin"] = tab.url;
-
-  chrome.storage.session.set({inputCards: input})
+  chrome.storage.session.set({"input": input})
     .then(()=>{
       console.log(`Storage set to ${input}`)
     })
   
-  chrome.storage.session.get(["inputCards"])
+  chrome.storage.session.get("input")
     .then((result) => {
-      console.log(result.textInput, "<")
+      console.log(result, "<")
     })
 }
 
--- popup/popup.js
+++ popup/popup.js
@@ -2,13 +2,54 @@
 // Get event for browser rightclick Q. 
 // Get event payload . . . 
 // Get 
-const flashcard = document.getElementById('flashcardAnswer')
-console.log(flashcard, "<< fc")
+const question = document.getElementById('flashcardAnswer')
+const answer = document.getElementById('flashcardPrompt')
 
 chrome.storage.session.get(["inputCards"])
     .then((result) => {
       console.log("Storage is currently" + result.textInput)
-      console.log(flashcard)
-      flashcard.value = "wah wah wah"
     })
+    
 
+const test = document.getElementById('testButton')
+
+
+getCards = () => {
+  chrome.storage.session.get(["input"])
+    .then(({input}) => {
+      console.log(input.text)
+      console.log(input.origin)
+
+
+      const card_type = "default";
+
+      if (input.text.length > 0 && input.text.length < 2000) {
+        fetch(`http://127.0.0.1:5000/flashcards`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            },
+          body: JSON.stringify({
+            "url": String(input.origin),
+            "type": card_type,
+            "text": String(input.text)
+            })
+          })
+          .then((res)=>{
+            return res.json()
+          })
+          .then(({flashcards})=>{
+            answer.value = flashcards[0].front
+            question.value = flashcards[0].back
+          })
+          .catch((err)=>{
+            console.log('error!:', err)
+        })
+      } else {
+        console.log("too long", input.text.length)
+      }
+
+    })
+}
+
+test.addEventListener("click", getCards)
