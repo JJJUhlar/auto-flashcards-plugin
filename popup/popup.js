f7bfc58ce63a98e2bcd49e4d05dd499f7c72d051
JJJUhlar <jjuhlar@gmail.com>
2023-05-17 10:46:34 +0100

    api req for update delete get practice cards

--- popup/popup.js
+++ popup/popup.js
@@ -6,6 +6,9 @@ const get = document.getElementById('getButton')
 const save = document.getElementById('saveCardsButton')
 const next = document.getElementById('nextCardButton')
 const previous = document.getElementById('previousCardButton')
+const getDueCardsButton = document.getElementById('getDueCardsButton')
+
+
 let currentCardIndex = 0;
 
 updateCardNum = () => {
@@ -58,8 +61,7 @@ changeCard = (e) => {
 sendCards = () => {
   chrome.storage.session.get("flashcards")
     .then(({flashcards}) => {
-      
-      fetch(`http://127.0.0.1:8080/new_cards`, {
+      fetch(`http://127.0.0.1:8080/save_cards`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -71,14 +73,77 @@ sendCards = () => {
       .then((res)=>{  
         alert("cards sent")
         console.log("cards sent", res.statusText)
-      })  
+      })
       .catch((err)=>{
         console.log("error:", err)
       })
     })
 }
 
+getDueCards = () => {
+  fetch(`http://127.0.0.1:8080/due_cards`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    }
+  })
+  .then((res)=>{
+    return res.json()
+  })
+  .then((data)=>{
+    console.log(data)
+    console.log(data.due_cards)
+    chrome.storage.session.set({"due_cards": data.due_cards})
+  })
+  .catch((err)=>{
+    console.log("error:", err)
+  })
+}
+
+updatePracticedCards = (practiced_flashcards) => {
+  fetch(`http://127.0.0.1:8080/update_cards`,{
+    method: "PATCH",
+    headers: { 
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      "cards_to_update": practiced_flashcards
+    })
+  })
+  .then((res)=>{
+    return res.json()
+  })
+  .then((data)=>{
+    console.log(data)
+  })
+  .catch((err)=>{
+    console.log("error:", err)
+  })
+}
+
+deleteCard = (card_to_delete_id) => {
+  fetch(`http://127.0.0.1:8080/delete_card`,{
+    method: "DELETE",
+    headers: { 
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      "card_to_delete_id": card_to_delete_id
+    })
+  })
+  .then((res)=>{
+    return res.json()
+  })
+  .then((data)=>{
+    console.log(data)
+  })
+  .catch((err)=>{
+    console.log("error:", err)
+  })
+}
+
 get.addEventListener("click", getCards)
 save.addEventListener('click', sendCards)
 next.addEventListener("click", changeCard)
 previous.addEventListener("click", changeCard)
+getDueCardsButton.addEventListener("click", getDueCards)
